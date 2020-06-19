6c63ff79a2d02b19b1fffc8f889565cdcb76b2d5
Kazi Faisal <doctr.kz@gmail.com>
2020-06-18 21:34:08 -0400

    Add files via upload

---
+++ AdventureGame.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AdventureGame : MonoBehaviour
+{
+    //[SerializeField] - adds the variable as a field into the unity inspector and allows dynamic manipulation of values
+    //Records texts components that are to be shown on the lcd display
+    [SerializeField] Text textComponent;
+
+    //To initialize the contents to be displayed on the lcd.
+    [SerializeField] State startingState;
+
+
+    State state;
+    // Start is called before the first frame update
+    void Start()
+    {
+        state = startingState;
+        textComponent.text = state.GetStateStory();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ManageState();
+    }
+
+    //This method, when called, will enable users to select an option using the number keys. The state will be changed based on the the selected option.
+    private void ManageState() {
+        var nextStates = state.GetNextStates();
+        for(int i = 0; i < nextStates.Length; i++) {
+            if(Input.GetKeyDown(KeyCode.A)) {
+                state = nextStates[0];
+            }
+            else if(Input.GetKeyDown(KeyCode.B)) {
+                state = nextStates[1];
+            }
+        }
+        textComponent.text = state.GetStateStory();
+    }
+}
---
+++ State.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//[CreateAssetMenu(menuName = "_NAME_")] is Unity specific code that essentially creates an asset for the given name for the game.
+[CreateAssetMenu(menuName = "State")]
+
+//Creating the state class allows holding variables to represent values of the current state and available states and functions for switching states, etc.
+public class State : ScriptableObject
+{
+    //Defining the placeholder for the text values to be displayed everytime a user enters a state
+    [TextArea(10,14)] [SerializeField] string storyText;
+
+    //Defining the placeholder to hold the subsequent states following the starting state
+    [SerializeField] State[] nextStates;
+
+    public string GetStateStory() {
+        return storyText;
+    }
+    public State[] GetNextStates() {
+        return nextStates;
+    }
+}
